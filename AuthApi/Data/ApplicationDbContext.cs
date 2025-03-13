b3cec204774e7b03924ae1bfb61395931fa44376
Manuel Salas Guisado <38837695+manuelsalas@users.noreply.github.com>
2025-03-13 20:57:35 -0300

    Add AuthApi/Data/ApplicationDbContext.cs 1741910255970

---
+++ AuthApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+using AuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.Username).IsUnique();
+            entity.HasIndex(e => e.Email).IsUnique();
+            entity.Property(e => e.Username).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.PasswordHash).IsRequired();
+        });
+    }
+}
